activate vrnn; add get_prior_params; fix variance

abstract_vae.py
@@ -9,11 +9,11 @@ import torch.nn.functional as F
 from copy import deepcopy
 from torch.autograd import Variable
 from collections import OrderedDict, Counter
-
 from helpers.pixel_cnn.model import PixelCNN
 from helpers.utils import same_type, zeros, nan_check_and_break, one_hot_np, get_dtype
 from helpers.layers import View, flatten_layers, Identity, EMA, \
-    build_pixelcnn_decoder, add_normalization, str_to_activ_module, get_decoder, get_encoder
+    build_pixelcnn_decoder, add_normalization, str_to_activ_module, \
+    get_decoder, get_encoder, build_volume_preserving_resnet, ResnetDeconvBlock, ResnetBlock, AttentionBlock
 from helpers.distributions import nll_activation as nll_activation_fn
 from helpers.distributions import nll as nll_fn
 from helpers.distributions import nll_has_variance
@@ -37,8 +37,16 @@ class VarianceProjector(nn.Module):
 
         # build the sequential layer
         if nll_has_variance(config['nll_type']):
+            # self.register_parameter(
+            #     "variance_scalar",
+            #     #nn.Parameter(torch.randn(config['batch_size'], self.chans, 1, 1) * 0.01)
+            #     #nn.Parameter(torch.zeros(config['batch_size'], self.chans, 1, 1))
+            #     #nn.Parameter(torch.zeros(config['batch_size'], self.chans, 1, 1))
+            #     nn.Parameter(torch.zeros(1, self.chans, 1, 1))
+            #     #nn.Parameter(torch.zeros(1))
+            # )
             if config['decoder_layer_type'] in ['conv', 'coordconv', 'resnet']:
-                self.decoder_projector = nn.Sequential(
+		self.decoder_projector = nn.Sequential(
                     activation_fn(),
                     # nn.ConvTranspose2d(chans, chans*2, 1, stride=1, bias=False)
                     nn.Conv2d(chans, chans*2, 1, stride=1, bias=False)
@@ -55,7 +63,13 @@ class VarianceProjector(nn.Module):
 
     def forward(self, x):
         if hasattr(self, 'decoder_projector'):
-            return self.decoder_projector(x)
+            assert x.dim() == 4, "got greater than 4d, why?"
+            decoded = self.decoder_projector(x)
+            half_chans = decoded.shape[1] // 2
+            return torch.cat([decoded[:, 0:half_chans, :, :],    # clamp the variance below with sigm
+                              decoded[:, half_chans:, :, :]], 1)
+                              # torch.sigmoid(decoded[:, half_chans:, :, :])], 1)
+                              # F.softplus(decoded[:, half_chans:, :, :], beta=1)], 1)
 
         return x
 
@@ -97,27 +111,84 @@ class AbstractVAE(nn.Module):
         :rtype: nn.Module
 
         """
-        conv_layer_types = ['conv', 'coordconv', 'resnet']
-        input_shape = [self.input_shape[0], 0, 0] if self.config['encoder_layer_type'] \
-            in conv_layer_types else self.input_shape
+        # conv_layer_types = ['conv', 'coordconv', 'resnet']
+        # input_shape = [self.input_shape[0], 0, 0] if self.config['encoder_layer_type'] \
+        #     in conv_layer_types else self.input_shape
 
         # return the encoder
-        return get_encoder(self.config)(input_shape=input_shape,
-                                        output_size=self.reparameterizer.input_size,
-                                        activation_fn=self.activation_fn)
+        # return get_encoder(self.config)(input_shape=input_shape,
+        #                                 output_size=self.reparameterizer.input_size,
+        #                                 activation_fn=self.activation_fn)
 
-    def lazy_build_decoder(self, input_size):
-        """ Lazily build the decoder network given the input_size
+        # return nn.Sequential(
+        #     nn.Conv2d(input_shape[0], self.config['filter_depth'], kernel_size=5, stride=1),
+        #     nn.GroupNorm(16, self.config['filter_depth']),
+        #     # nn.BatchNorm2d(self.config['filter_depth']),
+        #     nn.ELU(),
+        #     nn.Conv2d(self.config['filter_depth'], self.config['filter_depth']*2, kernel_size=4, stride=2),
+        #     nn.GroupNorm(32, self.config['filter_depth']*2),
+        #     # nn.BatchNorm2d(self.config['filter_depth']*2),
+        #     nn.ELU(),
+        #     nn.Conv2d(self.config['filter_depth']*2, self.config['filter_depth'], kernel_size=4, stride=1),
+        #     nn.GroupNorm(16, self.config['filter_depth']),
+        #     # nn.BatchNorm2d(self.config['filter_depth']),
+        #     nn.ELU(),
+        #     nn.Conv2d(self.config['filter_depth'], self.reparameterizer.input_size, kernel_size=1, stride=1)
+        #     # nn.Conv2d(256, 128, kernel_size=4, stride=2),
+        #     # nn.BatchNorm2d(128)
+        # )
 
-        :param input_size: the input size of the latent vector
-        :returns: a decoder
-        :rtype: nn.Module
+        # return nn.Sequential(
+        #     build_volume_preserving_resnet(input_shape, self.config['filter_depth'],
+        #                                    activation_fn=self.activation_fn,
+        #                                    num_layers=4,
+        #                                    normalization_str=self.config['conv_normalization']),
+        #     nn.Conv2d(input_shape[0], input_shape[0]*2, kernel_size=1)
+        #     # nn.Conv2d(input_shape[0], self.config['filter_depth'], ker nel_size=5, stride=1),
+        #     # nn.GroupNorm(16, self.config['filter_depth']),
+        #     # nn.ELU(),
+        #     # nn.Conv2d(self.config['filter_depth'], self.config['filter_depth']*2, kernel_size=4, stride=2),
+        #     # nn.GroupNorm(32, self.config['filter_depth']*2),
+        #     # nn.ELU(),
+        #     # nn.Conv2d(self.config['filter_depth']*2, self.config['filter_depth'], kernel_size=4, stride=1),
+        #     # nn.GroupNorm(16, self.config['filter_depth']),
+        #     # nn.ELU(),
+        #     # nn.Conv2d(self.config['filter_depth'], self.reparameterizer.input_size, kernel_size=1, stride=1)
+        # )
 
-        """
-        if not hasattr(self, 'decoder'):
-            setattr(self, 'decoder', self._build_decoder(input_size))
+        # ResnetBlock(3, 32, stride=1, normalization_str='groupnorm', downsample=True, activation_fn=nn.ELU),
+        # ResnetBlock(32, 64, stride=2, normalization_str='groupnorm', downsample=True, activation_fn=nn.ELU),
+        # ResnetBlock(64, 128, stride=1, normalization_str='groupnorm', downsample=True, activation_fn=nn.ELU),
+        # ResnetBlock(32, 2, stride=1, normalization_str='groupnorm', downsample=True, activation_fn=nn.ELU),
 
-        return self.decoder
+        # return nn.Sequential(
+        #     nn.Conv2d(3, 32, kernel_size=4, stride=2, padding=1),
+        #     nn.ELU(),
+        #     nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
+        #     nn.ELU(),
+        #     nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+        #     nn.ELU(),
+        #     ResnetBlock(64, 64, stride=1, normalization_str='none', downsample=True, activation_fn=nn.ELU),
+        #     ResnetBlock(64, 64, stride=1, normalization_str='none', downsample=True, activation_fn=nn.ELU),
+        #     ResnetBlock(64, 64, stride=1, normalization_str='none', downsample=True, activation_fn=nn.ELU),
+        #     nn.Conv2d(64, 1, kernel_size=1, stride=1)
+        # )
+
+
+        return nn.Sequential(
+            get_encoder(self.config)(input_shape=self.input_shape,
+                                     output_size=self.reparameterizer.input_size,
+                                     activation_fn=self.activation_fn),
+            # AttentionBlock(input_shape=self.reparameterizer.input_size,
+            #                embedding_size=self.reparameterizer.input_size*3, num_heads=3,
+            #                normalization_str='none'),
+            # AttentionBlock(input_shape=self.reparameterizer.input_size*3,
+            #                embedding_size=self.reparameterizer.input_size*3, num_heads=3,
+            #                normalization_str='none'),
+            # AttentionBlock(input_shape=self.reparameterizer.input_size*3,
+            #                embedding_size=self.reparameterizer.input_size, num_heads=1,
+            #                normalization_str='none'),
+        )
 
     def build_decoder(self, reupsample=True):
         """ helper function to build convolutional or dense decoder
@@ -131,9 +202,96 @@ class AbstractVAE(nn.Module):
                 or self.config['nll_type'] == "log_logistic_256", \
                 "pixelcnn only works with disc_mix_logistic or log_logistic_256"
 
-        decoder = get_decoder(self.config, reupsample)(input_size=self.reparameterizer.output_size,
-                                                       output_shape=self.input_shape,
-                                                       activation_fn=self.activation_fn)
+        chans = self.input_shape[0]
+
+        # decoder = nn.Sequential(
+        #     build_volume_preserving_resnet(self.input_shape, self.config['filter_depth'],
+        #                                    activation_fn=self.activation_fn,
+        #                                    num_layers=4,
+        #                                    normalization_str=self.config['conv_normalization']),
+        #     # # nn.Conv2d(self.input_shape[0], self.input_shape[0], kernel_size=1)
+
+        #     # nn.ConvTranspose2d(self.reparameterizer.output_size, self.config['filter_depth']*4, kernel_size=4),
+        #     # nn.GroupNorm(32, self.config['filter_depth']*4),
+        #     # nn.ELU(),
+
+        #     # nn.ConvTranspose2d(self.config['filter_depth']*4, self.config['filter_depth']*2, kernel_size=4),
+        #     # nn.GroupNorm(32, self.config['filter_depth']*2),
+        #     # nn.ELU(),
+
+        #     # nn.ConvTranspose2d(self.config['filter_depth']*2, self.config['filter_depth'], kernel_size=4, stride=2),
+        #     # nn.GroupNorm(16, self.config['filter_depth']),
+        #     # nn.ELU(),
+
+        #     # nn.ConvTranspose2d(self.config['filter_depth'], chans, kernel_size=5, stride=1),
+        #     # nn.ELU(),
+        #     # nn.Conv2d(chans, chans, kernel_size=1),
+        #     # nn.Upsample(size=self.input_shape[1:], mode='bilinear', align_corners=True)
+        # )
+
+        # decoder = nn.Sequential(
+        #     ResnetDeconvBlock(1, 32, stride=1, upsample=True, normalization_str='groupnorm', activation_fn=nn.ELU),
+        #     ResnetDeconvBlock(32, 64, stride=1, upsample=True, normalization_str='groupnorm', activation_fn=nn.ELU),
+        #     ResnetDeconvBlock(64, 32, stride=1, upsample=True, normalization_str='groupnorm', activation_fn=nn.ELU),
+        #     ResnetDeconvBlock(32, 3, stride=1, upsample=True, normalization_str='none', activation_fn=Identity),
+        # )
+
+        # decoder = nn.Sequential(
+        #     nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1),
+        #     ResnetBlock(64, 64, stride=1, normalization_str='none', downsample=True, activation_fn=nn.ELU),
+        #     ResnetBlock(64, 64, stride=1, normalization_str='none', downsample=True, activation_fn=nn.ELU),
+        #     ResnetBlock(64, 64, stride=1, normalization_str='none', downsample=True, activation_fn=nn.ELU),
+        #     nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+        #     nn.ELU(),
+        #     nn.ConvTranspose2d(32, 3, kernel_size=4, stride=2, padding=1)
+        # )
+
+        # decoder = nn.Sequential(
+        #     nn.ConvTranspose2d(self.reparameterizer.output_size, self.config['filter_depth']*4, kernel_size=4),
+        #     nn.GroupNorm(32, self.config['filter_depth']*4),
+        #     # nn.BatchNorm2d(self.config['filter_depth']*2),
+        #     nn.ELU(),
+
+        #     nn.ConvTranspose2d(self.config['filter_depth']*4, self.config['filter_depth']*2, kernel_size=4),
+        #     nn.GroupNorm(32, self.config['filter_depth']*2),
+        #     # nn.BatchNorm2d(self.config['filter_depth']*2),
+        #     nn.ELU(),
+
+        #     nn.ConvTranspose2d(self.config['filter_depth']*2, self.config['filter_depth'], kernel_size=4, stride=2),
+        #     nn.GroupNorm(16, self.config['filter_depth']),
+        #     # nn.BatchNorm2d(self.config['filter_depth']),
+        #     nn.ELU(),
+
+        #     nn.ConvTranspose2d(self.config['filter_depth'], chans, kernel_size=5, stride=1),
+        #     # nn.BatchNorm2d(chans),
+        #     nn.ELU(),
+        #     nn.Conv2d(chans, chans, kernel_size=1),
+        #     nn.Upsample(size=self.input_shape[1:], mode='bilinear', align_corners=True)
+        # )
+
+        # decoder = get_decoder(self.config, reupsample)(input_size=self.reparameterizer.output_size,
+        #                                                output_shape=self.input_shape,
+        #                                                activation_fn=self.activation_fn)
+
+        dec_conf = deepcopy(self.config)
+        if dec_conf['nll_type'] == 'pixel_wise':
+            dec_conf['input_shape'][0] *= 256
+
+        decoder = nn.Sequential(
+            # AttentionBlock(input_shape=self.reparameterizer.output_size,
+            #                embedding_size=self.reparameterizer.output_size*3, num_heads=3,
+            #                normalization_str='none'),
+            # AttentionBlock(input_shape=self.reparameterizer.output_size*3,
+            #                embedding_size=self.reparameterizer.output_size*3, num_heads=3,
+            #                normalization_str='none'),
+            # AttentionBlock(input_shape=self.reparameterizer.output_size*3,
+            #                embedding_size=self.reparameterizer.output_size, num_heads=1,
+            #                normalization_str='none'),
+            get_decoder(dec_conf, reupsample)(input_size=self.reparameterizer.output_size,
+                                              output_shape=dec_conf['input_shape'],
+                                              activation_fn=self.activation_fn)
+        )
+
         # append the variance as necessary
         return self._append_variance_projection(decoder)
 
@@ -402,7 +560,56 @@ class AbstractVAE(nn.Module):
         params = self._compute_mi_params(decoded_logits, params)
         return decoded_logits, params
 
-    def loss_function(self, recon_x, x, params):
+    def importance_weighted_elbo_analytic(self, x, K=5000):
+        """ ELBO using importance samples.
+
+        :param x: input data.
+        :param K: number of importance samples.
+        :returns: negative ELBO
+        :rtype: float32
+
+        """
+        elbo_mu = torch.zeros(x.shape[0], device=x.device)
+
+        for _ in tqdm(range(K)):
+            self.reparameterize(params['z'], force=True)
+            decoded_logits, params = self(x)
+            loss_t = self.loss_function(decoded_logits, x, params)
+            elbo_mu += loss_t['elbo']
+
+        # log-sum exp and return
+        return torch.logsumexp(elbo_mu / K, dim=0)
+
+    def importance_weighted_elbo_monte_carlo(self, x, K=500):
+        """ ELBO using importance samples.
+
+        :param x: input data.
+        :param K: number of importance samples.
+        :returns: negative ELBO
+        :rtype: float32
+
+        """
+        elbo_mu = torch.zeros(x.shape[0], device=x.device)
+        from tqdm import tqdm
+
+        for _ in tqdm(range(K)):
+            z, params = self.posterior(x, force=True)
+            decoded_logits = self.decode(z, x=x)
+
+            # grab log likelihood of posterior and prior over z
+            log_q_z_given_x = self.reparameterizer.log_likelihood(z, params)
+            prior_params = self.reparameterizer.prior_params(x.shape[0])
+            log_p_z = self.reparameterizer.log_likelihood(z, prior_params)
+
+            # compute the NLL and the log_q_z_given_x - log_p_z
+            nll = nll_fn(x, decoded_logits, self.config['nll_type'])
+            kl_z = torch.sum(log_q_z_given_x - log_p_z, -1)
+            elbo_mu += nll - kl_z
+
+        # log-sum exp and return
+        return torch.logsumexp(elbo_mu / K, dim=0)
+
+    def loss_function(self, recon_x, x, params, K=1):
         """ Produces ELBO, handles mutual info and proxy loss terms too.
 
         :param recon_x: the unactivated reconstruction preds.
@@ -418,6 +625,16 @@ class AbstractVAE(nn.Module):
         #     x = (x + .5) / 256.
 
         nll = nll_fn(x, recon_x, self.config['nll_type'])
+
+        # multiple monte-carlo samples for the decoder.
+        if self.training:
+            for k in range(1, K):
+                z_k, params_k = self.reparameterize(params['logits'])
+                recon_x_i = self.decode(z_k)
+                nll = nll + nll_fn(x, recon_x_i, self.config['nll_type'])
+
+            nll = nll / K
+
         kld = self.kld(params)
         elbo = nll + kld  # save the base ELBO, but use the beta-vae elbo for the full loss
 
@@ -432,9 +649,11 @@ class AbstractVAE(nn.Module):
         # handle the mutual information term
         mut_info = self.mut_info(params, x.size(0))
 
+        # compute full loss
         loss = (nll + self.config['kl_beta'] * kld) - mut_info
         return {
             'loss': loss,
+            'elbo': elbo,
             'loss_mean': torch.mean(loss),
             'elbo_mean': torch.mean(elbo),
             'nll_mean': torch.mean(nll),
@@ -463,15 +682,16 @@ class AbstractVAE(nn.Module):
         """
         return self.reparameterizer.get_reparameterizer_scalars()
 
-    def reparameterize(self, logits):
+    def reparameterize(self, logits, force=False):
         """ Reparameterize the logits and returns a dict.
 
         :param logits: unactivated encoded logits.
+        :param force: force reparameterize the distributions
         :returns: reparam dict
         :rtype: dict
 
         """
-        return self.reparameterizer(logits)
+        return self.reparameterizer(logits, force=force)
 
     def decode(self, z, x=None):
         """ Decode a latent z back to x.
@@ -499,19 +719,18 @@ class AbstractVAE(nn.Module):
 
         return decoded_logits
 
-    def posterior(self, x):
+    def posterior(self, x, force=False):
         """ get a reparameterized Q(z|x) for a given x
 
         :param x: input tensor
+        :param force:  force reparameterization
         :returns: reparam dict
         :rtype: torch.Tensor
 
         """
         z_logits = self.encode(x)               # encode logits
-        if self.training:
-            self.aggregate_posterior(z_logits)  # aggregate posterior
-
-        return self.reparameterize(z_logits)    # return reparameterized value
+        self.aggregate_posterior(z_logits)      # aggregate posterior EMA
+        return self.reparameterize(z_logits, force=force)    # return reparameterized value
 
     def encode(self, x):
         """ Encodes a tensor x to a set of logits.
@@ -526,7 +745,11 @@ class AbstractVAE(nn.Module):
             x = (x + .5) / 256.0
 
         # print('[ORIG] x max = ', x.max(), " min = ", x.min())
-        return self.encoder(x)
+        encoded = self.encoder(x)
+        if encoded.dim() < 2:
+            return encoded.unsqueeze(-1)
+
+        return encoded
 
     def kld(self, dist_a):
         """ KL-Divergence of the distribution dict and the prior of that distribution.
@@ -582,8 +805,7 @@ class AbstractVAE(nn.Module):
         mut_info = same_type(self.config['half'], self.config['cuda'])(batch_size).zero_()
 
         # only grab the mut-info if the scalars above are set
-        if (self.config['continuous_mut_info'] > 0
-             or self.config['discrete_mut_info'] > 0):
+        if self.config['continuous_mut_info'] > 0 or self.config['discrete_mut_info'] > 0:
             mut_info = self._clamp_mut_info(self.reparameterizer.mutual_info(dist_params))
 
         return mut_info

reparameterizers/bernoulli.py
@@ -21,14 +21,35 @@ class Bernoulli(nn.Module):
         self.output_size = self.config['discrete_size']
         self._setup_anneal_params()
 
-    def _prior_distribution(self, batch_size):
-        uniform_probs = float_type(self.config['cuda'])(batch_size, self.output_size).zero_()
-        #uniform_probs += 1.0 / self.output_size
+    def prior_params(self, batch_size, **kwargs):
+        """ Helper to get prior parameters
+
+        :param batch_size: the size of the batch
+        :returns: a dictionary of parameters
+        :rtype: dict
+
+        """
+        uniform_probs = same_type(self.config['half'], self.config['cuda'])(1, self.output_size).zero_()
         uniform_probs += 0.5
-        return torch.distributions.Bernoulli(probs=uniform_probs)
+        return {
+            'discrete':{
+                'logits': D.Bernoulli(probs=uniform_probs).logits
+            }
+        }
+
+    def prior_distribution(self, batch_size, **kwargs):
+        """ get a torch distrbiution prior
+
+        :param batch_size: size of the prior
+        :returns: uniform categorical
+        :rtype: torch.distribution
+
+        """
+        params = self.prior_params(batch_size, **kwargs)
+        return torch.distributions.Bernoulli(logits=params['discrete']['logits'])
 
     def prior(self, batch_size, **kwargs):
-        return self._prior_distribution(batch_size).sample()
+        return self.prior_distribution(batch_size, **kwargs).sample()
 
     def _setup_anneal_params(self):
         """setup the annealing parameters; TODO: parameterize
@@ -37,11 +58,44 @@ class Bernoulli(nn.Module):
         :rtype: None
 
         """
-        self.min_temp = 0.3
+        # for regular annealing
+        self.anneal_rate = 3e-6
+        self.tau0 = 1.0
+
+        # for cos anneal
+        self.min_temp = 1e-6
         self.max_temp = 1.0
         self.tau = 1.0
         self.last_epoch = self.config['epochs']
         self.clip_min = 1e-8
+
+    def get_reparameterizer_scalars(self):
+        """ Returns any scalars used in reparameterization.
+
+        :returns: dict of scalars
+        :rtype: dict
+
+        """
+        return {'tau_scalar': self.tau}
+
+    def anneal(self, anneal_interval=10):
+        """ Helper to anneal the temperature.
+
+        :param anneal_interval: the interval to employ annealing.
+        :returns: None
+        :rtype: None
+
+        """
+        if self.training \
+           and self.iteration > 0 \
+           and self.iteration % anneal_interval == 0:
+
+            # smoother annealing
+            rate = -self.anneal_rate * self.iteration
+            self.tau = np.maximum(self.tau0 * np.exp(rate),
+                                  self.min_temp)
+            # hard annealing
+            # self.tau = np.maximum(0.9 * self.tau, self.min_temp)
 
     def cosine_anneal(self):
         """ consine-anneals the temperature
@@ -55,6 +109,21 @@ class Bernoulli(nn.Module):
                 * (1 + math.cos(math.pi * -1 / self.last_epoch)) / 2
             self.tau = np.clip(updated_tau, self.clip_min, self.max_temp)
 
+    def _compute_hard(self, relaxed):
+        """ helper to get the hard version of relaxed
+
+        :param relaxed: the relaxed estimate
+        :returns: hard bernoulli with 0s and 1s
+        :rtype: torch.Tensor
+
+        """
+
+        hard = relaxed.clone()
+        hard[relaxed < 0.5] = 0.0
+        hard[relaxed >= 0.5] = 1.0
+        hard_diff = hard - relaxed # sub the relaxed tensor backprop path
+        return hard_diff.detach() + relaxed # add back for 0 effect, keeping bp path
+
     def reparmeterize(self, logits):
         """ reparamterize the logits
 
@@ -64,9 +133,7 @@ class Bernoulli(nn.Module):
 
         """
         relaxed = D.RelaxedBernoulli(temperature=self.tau, logits=logits).rsample()
-        hard = relaxed.clone()
-        hard[relaxed < 0.5] = 0.0
-        hard[relaxed >= 0.5] = 1.0
+        hard = self._compute_hard(relaxed)
         return relaxed, hard
 
     def mutual_info_analytic(self, params, eps=1e-9):
@@ -79,40 +146,59 @@ class Bernoulli(nn.Module):
         raise NotImplementedError
 
     @staticmethod
-    def _kld_bern_uniform(q_z, dim=-1, eps=1e-9):
-        return torch.mul(q_z, torch.log(q_z + eps) - np.log(0.5)) + \
-            torch.mul(1.0 - q_z, torch.log(1 - q_z + eps) - np.log(0.5))
+    def _kld_bern_bern(q_dist, p_dist, eps=1e-6):
+        """ Stable bern-bern KL from: https://github.com/pytorch/pytorch/issues/15288
 
-    @staticmethod
-    def _kld_bern_bern(q_z, p_z, dim=-1, eps=1e-9):
-        return torch.mul(q_z, torch.log(q_z + eps) - torch.log(p_z + eps)) + \
-            torch.mul(1.0 - q_z, torch.log(1 - q_z + eps) - torch.log(p_z + eps))
+        :param q_dist: the D.Bernoulli q distribution
+        :param p_dist: the D.Bernoulli p distribution
+        :param eps: numerical precision
+        :returns: kl-divergence (non-reduced)
+        :rtype: torch.Tensor
 
-    def kl(self, dist_a, prior=None, eps=1e-9):
+        """
+        q1 = q_dist.probs
+        q0 = 1 - q1
+        p1 = p_dist.probs
+        p0 = 1 - p1
+
+        logq1 = (q1 + eps).log()
+        logq0 = (q0 + eps).log()
+        logp1 = (p1 + eps).log()
+        logp0 = (p0 + eps).log()
+
+        kldiv_1 = q1*(logq1 - logp1)
+        kldiv_0 = q0*(logq0 - logp0)
+        return kldiv_1 + kldiv_0
+
+    def kl(self, dist_a, prior=None, eps=1e-6):
+        """ Evaluates KL between two distributions or against a naive prior if not provided
+
+        :param dist_a: the dist in KL(dist_a, prior)
+        :param prior: the (optional) prior in KL(dist_a, prior)
+        :param eps: tolerance for numerical precision
+        :returns: [batch_size] size tensor
+        :rtype: torch.Tensor
+
+        """
+        batch_size = dist_a['discrete']['logits'].shape[0]
+
         if prior is None:  # use standard uniform prior
-            # prior = self._prior_distribution(dist_a['discrete']['logits'].shape[0])
-            # bern = torch.distributions.Bernoulli(logits=dist_a['discrete']['logits'])
-            # return torch.sum(D.kl_divergence(bern, prior), -1)
+            prior_probs = torch.zeros_like(dist_a['discrete']['logits']) + 0.5
+            prior = D.Bernoulli(probs=prior_probs)
+            bern = D.Bernoulli(logits=dist_a['discrete']['logits'])
+            kld_elem = self._kld_bern_bern(bern, prior)
+            return torch.sum(kld_elem, -1)
 
-            return torch.sum(Bernoulli._kld_bern_uniform(
-                #torch.log(F.sigmoid(dist_a['discrete']['logits'] + eps) + eps)), -1)
-                F.sigmoid(dist_a['discrete']['logits'])), -1)
-
-            # bern = torch.distributions.RelaxedBernoulli(temperature=self.tau, logits=dist_a['discrete']['logits'])
-            # return torch.sum(F.kl_div(bern.rsample(), prior.sample(), reduction='none'), -1)
-
-        # we have two distributions provided (eg: VRNN)
-        kld_elem = Bernoulli._kld_bern_bern(F.sigmoid(dist_a['discrete']['logits']),
-                                            F.sigmoid(prior['discrete']['logits']))
+        kld_elem = self._kld_bern_bern(D.Bernoulli(logits=dist_a['discrete']['logits']),
+                                       D.Bernoulli(logits=prior['discrete']['logits']))
         return torch.sum(kld_elem, -1)
-
-        raise NotImplementedError
 
     def log_likelihood(self, z, params):
         return D.Bernoulli(logits=params['discrete']['logits']).log_prob(z)
 
     def forward(self, logits, force=False):
-        self.cosine_anneal()  # anneal first
+        #self.cosine_anneal()  # anneal first
+        self.anneal()
         z, z_hard = self.reparmeterize(logits)
         params = {
             'z_hard': z_hard,

reparameterizers/gumbel.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 import torch.distributions as D
 from torch.autograd import Variable
 
-from helpers.utils import float_type, one_hot, ones_like, long_type
+from helpers.utils import same_type, one_hot, ones_like, long_type, get_dtype
 
 
 class GumbelSoftmax(nn.Module):
@@ -28,6 +28,34 @@ class GumbelSoftmax(nn.Module):
         self.input_size = self.config['discrete_size']
         self.output_size = self.config['discrete_size']
 
+    def prior_params(self, batch_size, **kwargs):
+        """ Helper to get prior parameters
+
+        :param batch_size: the size of the batch
+        :returns: a dictionary of parameters
+        :rtype: dict
+
+        """
+        uniform_probs = same_type(self.config['half'], self.config['cuda'])(
+            batch_size, self.output_size).zero_()
+        uniform_probs += 1.0 / self.output_size
+        return {
+            'discrete':{
+                'logits': D.OneHotCategorical(probs=uniform_probs).logits
+            }
+        }
+
+    def prior_distribution(self, batch_size, **kwargs):
+        """ get a torch distrbiution prior
+
+        :param batch_size: size of the prior
+        :returns: uniform categorical
+        :rtype: torch.distribution
+
+        """
+        params = self.prior_params(batch_size, **kwargs)
+        return D.OneHotCategorical(logits=params['discrete']['logits'])
+
     def prior(self, batch_size, **kwargs):
         """ Sample the prior for batch_size samples.
 
@@ -36,13 +64,8 @@ class GumbelSoftmax(nn.Module):
         :rtype: torch.Tensor
 
         """
-        uniform_probs = float_type(self.config['cuda'])(1, self.output_size).zero_()
-        uniform_probs += 1.0 / self.output_size
-        cat = torch.distributions.Categorical(uniform_probs)
-        sample = cat.sample((batch_size,))
-        return Variable(
-            one_hot(self.output_size, sample, use_cuda=self.config['cuda'])
-        ).type(float_type(self.config['cuda']))
+        cat = self.prior_distribution(batch_size, **kwargs)
+        return cat.sample()
 
     def get_reparameterizer_scalars(self):
         """ Returns any scalars used in reparameterization.
@@ -109,8 +132,10 @@ class GumbelSoftmax(nn.Module):
         :rtype: torch.Tensor
 
         """
-        targets = torch.argmax(params['discrete']['z_hard'].type(long_type(self.config['cuda'])), dim=-1)
-        crossent_loss = -F.cross_entropy(input=params['q_z_given_xhat']['discrete']['logits'],
+        targets = torch.argmax(
+            F.softmax(params['discrete']['logits'], -1), dim=-1
+        ).type(long_type(self.config['cuda']))
+        crossent_loss = F.cross_entropy(input=params['q_z_given_xhat']['discrete']['logits'],
                                          target=targets, reduce=False)
         ent_loss = -torch.sum(D.OneHotCategorical(logits=params['discrete']['z_hard']).entropy(), -1)
         return ent_loss + crossent_loss
@@ -125,12 +150,10 @@ class GumbelSoftmax(nn.Module):
         :rtype: torch.Tensor
 
         """
-        log_q_z_given_x = params['q_z_given_xhat']['discrete']['logits'] + eps
-        # log_q_z_given_x = params['discrete']['log_q_z'] + eps
-        p_z = self.prior(log_q_z_given_x.size()[0])
-        # p_z = params['discrete']['z_soft'] + eps
-        crossent_loss = -torch.sum(log_q_z_given_x * p_z, dim=-1)
-        ent_loss = -torch.sum(torch.log(p_z + eps) * p_z, dim=-1)
+        log_q_z_given_x_hat = params['q_z_given_xhat']['discrete']['log_q_z']
+        p_z = F.softmax(params['discrete']['logits'], dim=-1)
+        crossent_loss = torch.sum(p_z * log_q_z_given_x_hat, dim=-1)
+        ent_loss = -torch.sum(p_z * params['discrete']['log_q_z'], dim=-1)
         return ent_loss + crossent_loss
 
     def mutual_info(self, params, eps=1e-9):
@@ -142,15 +165,42 @@ class GumbelSoftmax(nn.Module):
         :rtype: torch.Tensor
 
         """
-        targets = torch.argmax(params['discrete']['z_hard'].type(long_type(self.config['cuda'])), dim=-1)
+        return self.config['discrete_mut_info']*self.mutual_info_monte_carlo(params)
+
+        targets = torch.argmax(params['q_z_given_xhat']['discrete']['z_hard'].type(
+            long_type(self.config['cuda'])), dim=-1)
         # soft_targets = F.softmax(
         #     params['discrete']['logits'], -1
         # ).type(long_type(self.config['cuda']))
         # targets = torch.argmax(params['discrete']['log_q_z'], -1) # 3rd change, havent tried
-        crossent_loss = -F.cross_entropy(input=params['q_z_given_xhat']['discrete']['logits'],
+        #crossent_loss = -F.cross_entropy(input=params['q_z_given_xhat']['discrete']['logits'],
+        crossent_loss = -F.cross_entropy(input=params['discrete']['logits'],
                                          target=targets, reduce=False)
-        ent_loss = -torch.sum(D.OneHotCategorical(logits=params['discrete']['z_hard']).entropy(), -1)
-        return self.config['discrete_mut_info'] * (ent_loss + crossent_loss)
+        # ent_loss = -torch.sum(D.OneHotCategorical(logits=params['discrete']['z_hard']).entropy(), -1)
+        #ent_loss = torch.sum(D.OneHotCategorical(logits=params['discrete']['z_hard']).entropy(), -1)
+        #print('xent = ', crossent_loss.shape, " |ent = ", ent_loss.shape)
+        ent_loss = -torch.sum(D.OneHotCategorical(logits=params['discrete']['logits']).entropy(), -1)
+        return -self.config['discrete_mut_info'] * (ent_loss + crossent_loss)
+        # return self.config['discrete_mut_info'] * crossent_loss
+
+    # def mutual_info(self, params, eps=1e-9):
+    #     """ Returns Ent + xent where xent is taken against hard targets.
+
+    #     :param params: distribution parameters
+    #     :param eps: tolerance
+    #     :returns: batch_size tensor of mutual info
+    #     :rtype: torch.Tensor
+
+    #     """
+    #     targets = torch.argmax(params['discrete']['z_hard'].type(long_type(self.config['cuda'])), dim=-1)
+    #     # soft_targets = F.softmax(
+    #     #     params['discrete']['logits'], -1
+    #     # ).type(long_type(self.config['cuda']))
+    #     # targets = torch.argmax(params['discrete']['log_q_z'], -1) # 3rd change, havent tried
+    #     crossent_loss = -F.cross_entropy(input=params['q_z_given_xhat']['discrete']['logits'],
+    #                                      target=targets, reduce=False)
+    #     ent_loss = -torch.sum(D.OneHotCategorical(logits=params['discrete']['z_hard']).entropy(), -1)
+    #     return self.config['discrete_mut_info'] * (ent_loss + crossent_loss)
 
     @staticmethod
     def _kld_categorical_uniform(log_q_z, dim=-1, eps=1e-9):
@@ -169,6 +219,15 @@ class GumbelSoftmax(nn.Module):
         kld_element = log_q_z.exp() * (log_q_z - log_p_z)
         return kld_element
 
+
+    @staticmethod
+    def _kl_tf_version(a, b):
+        ''' nn_ops.softmax(a.logits)*(nn_ops.log_softmax(a.logits)
+            - nn_ops.log_softmax(b.logits))
+        '''
+        return F.softmax(a.logits, dim=-1)*(F.log_softmax(a.logits, dim=-1)
+                                            - F.log_softmax(b.logits, dim=-1))
+
     def kl(self, dist_a, prior=None):
         """ KL divergence of dist_a against a prior, if none then Cat(1/k)
 
@@ -179,15 +238,21 @@ class GumbelSoftmax(nn.Module):
 
         """
         if prior == None:  # use standard uniform prior
-            return torch.sum(GumbelSoftmax._kld_categorical_uniform(
-                dist_a['discrete']['log_q_z'], dim=self.dim
+            # return torch.sum(GumbelSoftmax._kld_categorical_uniform(
+            #     dist_a['discrete']['log_q_z'], dim=self.dim
+            # ), -1)
+            prior = D.OneHotCategorical(logits=dist_a['discrete']['log_q_z'])
+            return torch.sum(GumbelSoftmax._kl_tf_version(
+                D.OneHotCategorical(logits=dist_a['discrete']['log_q_z']),
+                prior
             ), -1)
 
         # we have two distributions provided (eg: VRNN)
-        return D.kl_divergence(
+        return torch.sum(GumbelSoftmax._kl_tf_version(
             D.OneHotCategorical(logits=dist_a['discrete']['log_q_z']),
-            D.OneHotCategorical(prior['discrete']['log_q_z'])
-        )
+            #D.OneHotCategorical(prior['discrete']['log_q_z'])
+            D.OneHotCategorical(logits=prior['discrete']['log_q_z'])
+        ), -1)
 
 
     @staticmethod
@@ -232,7 +297,7 @@ class GumbelSoftmax(nn.Module):
         if hard:
             y_max, _ = torch.max(y, dim=dim, keepdim=True)
             y_hard = Variable(
-                torch.eq(y_max.data, y.data).type(float_type(use_cuda))
+                torch.eq(y_max.data, y.data).type(x.dtype)
             )
             y_hard_diff = y_hard - y
             y_hard = y_hard_diff.detach() + y
@@ -249,7 +314,7 @@ class GumbelSoftmax(nn.Module):
         :rtype: torch.Tensor
 
         """
-        return D.Categorical(logits=params['discrete']['logits']).log_prob(z)
+        return D.OneHotCategorical(logits=params['discrete']['logits']).log_prob(z)
 
     def forward(self, logits, force=False):
         """ Returns a reparameterized categorical and it's params.

reparameterizers/isotropic_gaussian.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 
 from helpers.utils import zeros_like, ones_like, same_type, \
-    float_type, nan_check_and_break, is_half
+    float_type, nan_check_and_break, is_half, zeros
 from helpers.utils import eps as eps_fn
 
 
@@ -25,6 +25,42 @@ class IsotropicGaussian(nn.Module):
         self.input_size = self.config['continuous_size']
         assert self.config['continuous_size'] % 2 == 0
         self.output_size = self.config['continuous_size'] // 2
+
+    def prior_params(self, batch_size, **kwargs):
+        """ Helper to get prior parameters
+
+        :param batch_size: the size of the batch
+        :returns: a dictionary of parameters
+        :rtype: dict
+
+        """
+        mu = same_type(self.config['half'], self.config['cuda'])(
+            batch_size, self.output_size
+        ).zero_() # zero mean
+
+        # variance = 1 unless otherwise specified
+        scale_var = 1.0 if 'scale_var' not in kwargs else kwargs['scale_var']
+        sigma = same_type(self.config['half'], self.config['cuda'])(
+            batch_size, self.output_size
+        ).zero_() + scale_var
+
+        return {
+            'gaussian': {
+                'mu': mu,
+                'logvar': sigma
+            }
+        }
+
+    def prior_distribution(self, batch_size, **kwargs):
+        """ get a torch distrbiution prior
+
+        :param batch_size: size of the prior
+        :returns: isotropic gaussian prior
+        :rtype: torch.distribution
+
+        """
+        params = self.prior_params(batch_size, **kwargs)
+        return D.Normal(params['gaussian']['mu'], params['gaussian']['logvar'])
 
     def prior(self, batch_size, **kwargs):
         """ Sample the prior for batch_size samples.
@@ -57,10 +93,11 @@ class IsotropicGaussian(nn.Module):
             ).normal_()
             eps = Variable(eps)
             nan_check_and_break(logvar, "logvar")
-            return eps.mul(std).add_(mu), {'mu': mu, 'logvar': logvar}
+            reparam_sample = eps.mul(std).add_(mu)
+            return reparam_sample, {'z': reparam_sample, 'mu': mu, 'logvar': logvar}
             # return D.Normal(mu, logvar).rsample(), {'mu': mu, 'logvar': logvar}
 
-        return mu, {'mu': mu, 'logvar': logvar}
+        return mu, {'z': mu, 'mu': mu, 'logvar': logvar}
 
     def reparmeterize(self, logits, force=False):
         """ Given logits reparameterize to a gaussian using
@@ -75,7 +112,8 @@ class IsotropicGaussian(nn.Module):
 
         if logits.dim() == 2:
             feature_size = logits.size(-1)
-            assert feature_size % 2 == 0 and feature_size // 2 == self.output_size
+            assert feature_size % 2 == 0 and feature_size // 2 == self.output_size, \
+                "feature_size = {}".format(feature_size)
             mu = logits[:, 0:int(feature_size/2)]
             nan_check_and_break(mu, "mu")
             sigma = logits[:, int(feature_size/2):] + eps
@@ -88,7 +126,8 @@ class IsotropicGaussian(nn.Module):
             sigma = logits[:, :, int(feature_size/2):] + eps
         elif logits.dim() == 4:
             feature_size = logits.size(1)
-            assert feature_size % 2 == 0 and feature_size // 2 == self.output_size
+            assert feature_size % 2 == 0 and feature_size // 2 == self.output_size, \
+                "feature_size = {}".format(feature_size)
             mu = logits[:, 0:int(feature_size/2), :, :]
             sigma = logits[:, int(feature_size/2):, :, :] + eps
         else:
@@ -114,11 +153,11 @@ class IsotropicGaussian(nn.Module):
         :rtype: torch.Tensor
 
         """
-        z_true = D.Normal(params['gaussian']['mu'],
+        z_given_x = D.Normal(params['gaussian']['mu'],
                           params['gaussian']['logvar'])
-        z_match = D.Normal(params['q_z_given_xhat']['gaussian']['mu'],
+        z_given_xhat = D.Normal(params['q_z_given_xhat']['gaussian']['mu'],
                            params['q_z_given_xhat']['gaussian']['logvar'])
-        kl_proxy_to_xent = torch.sum(D.kl_divergence(z_match, z_true), dim=-1)
+        kl_proxy_to_xent = torch.sum(D.kl_divergence(z_given_x, z_given_xhat), dim=-1)
         return self.config['continuous_mut_info'] * kl_proxy_to_xent
 
     @staticmethod
@@ -164,8 +203,8 @@ class IsotropicGaussian(nn.Module):
         :rtype: torch.Tensor
 
         """
-        return D.Normal(params['gaussian']['mu'],
-                        params['gaussian']['logvar']).log_prob(z)
+        return -(0.5 * np.log(2 * np.pi) + params['gaussian']['logvar']) \
+            - 0.5 * ((z - params['gaussian']['mu']) / torch.exp(params['gaussian']['logvar'])) ** 2
 
     def forward(self, logits, force=False):
         """ Returns a reparameterized gaussian and it's params.

reparameterizers/mixture.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-from helpers.utils import float_type, ones_like
+from helpers.utils import float_type, ones_like, nan_check_and_break
 from .beta import Beta
 from .gumbel import GumbelSoftmax
 from .isotropic_gaussian import IsotropicGaussian
@@ -40,6 +40,36 @@ class Mixture(nn.Module):
         """
         return self.discrete.get_reparameterizer_scalars()
 
+    def prior_params(self, batch_size, **kwargs):
+        """ Helper to get prior parameters
+
+        :param batch_size: the size of the batch
+        :returns: a dictionary of parameters
+        :rtype: dict
+
+        """
+        cont_params = self.continuous.prior_params(batch_size, **kwargs)
+        disc_params = self.discrete.prior_params(batch_size, **kwargs)
+        return {
+            **disc_params,
+            **cont_params
+        }
+
+    def prior_distribution(self, batch_size, **kwargs):
+        """ get a torch distrbiution prior
+
+        :param batch_size: size of the prior
+        :returns: uniform categorical
+        :rtype: torch.distribution
+
+        """
+        disc_dist = self.discrete.prior_distribution(batch_size, **kwargs)
+        cont_dist = self.continuous.prior_distribution(batch_size, **kwargs)
+        return {
+            'continuous': cont_dist,
+            'discrete': disc_dist
+        }
+
     def prior(self, batch_size, **kwargs):
         disc = self.discrete.prior(batch_size, **kwargs)
         cont = self.continuous.prior(batch_size, **kwargs)
@@ -53,6 +83,13 @@ class Mixture(nn.Module):
     def log_likelihood(self, z, params):
         cont = self.continuous.log_likelihood(z[:, 0:self.continuous.output_size], params)
         disc = self.discrete.log_likelihood(z[:, self.continuous.output_size:], params)
+        if disc.dim() < 2:
+            disc = disc.unsqueeze(-1)
+
+        # sanity check and return
+        nan_check_and_break(cont, 'cont_ll')
+        nan_check_and_break(disc, 'disc_ll')
+
         return torch.cat([cont, disc], 1)
 
     def reparmeterize(self, logits, force=False):

vrnn.py
@@ -89,8 +89,7 @@ class VRNNMemory(nn.Module):
         """
         self.memory_buffer.clear()
 
-    def init_state(self, batch_size, cuda=False,
-                   override_noisy_state=False):
+    def init_state(self, batch_size, cuda=False, override_noisy_state=False):
         """ Initializes (or re-initializes) the state.
 
         :param batch_size: size of batch
@@ -387,8 +386,13 @@ class VRNN(AbstractVAE):
             assert self.config['nll_type'] == "disc_mix_logistic", \
                 "pixelcnn only works with disc_mix_logistic"
 
-        decoder = get_decoder(self.config, reupsample)(input_size=self.config['latent_size']*2,
-                                                       output_shape=self.input_shape,
+        # create a dupe config
+        dec_conf = deepcopy(self.config)
+        if dec_conf['nll_type'] == 'pixel_wise':
+            dec_conf['input_shape'][0] *= 256
+
+        decoder = get_decoder(dec_conf, reupsample)(input_size=self.config['latent_size']*2,
+                                                       output_shape=dec_conf['input_shape'],
                                                        activation_fn=self.activation_fn,
                                                        reupsample=True)
         # append the variance as necessary
@@ -533,7 +537,7 @@ class VRNN(AbstractVAE):
                 decode_t, params_t = self.step(input_t[i])
             else:                          # single input encoded many times
                 decode_t, params_t = self.step(input_t)
-                input_t = decode_t
+                input_t = nll_activation_fn(decode_t, self.config['nll_type'])
 
             if self.training and i == 0:
                 self.aggregate_posterior['rnn_hidden_state_h'](self.memory.get_state()[0])
@@ -742,8 +746,10 @@ class VRNN(AbstractVAE):
 
         """
         if 'reset_state' in kwargs and kwargs['reset_state']:
-            self.memory.init_state(batch_size, cuda=self.config['cuda'])# ,
-                                   # override_noisy_state=True)
+            override_noisy_state = kwargs.get('override_noisy_state', False)
+            self.memory.init_state(batch_size, cuda=self.config['cuda'],
+                                   override_noisy_state=override_noisy_state)
+
         # grab final state and prior samples & encode them through feature extractor
         final_state, z_prior_t = self._get_prior_and_state(batch_size, **kwargs)
         phi_z_t = self.phi_z(z_prior_t)
@@ -756,6 +762,7 @@ class VRNN(AbstractVAE):
         # iterate max_time_steps -1  times using the output from above
         for _ in range(self.config['max_time_steps'] - 1):
             dec_output_t, _ = self.step(dec_output_t, **kwargs)
+            dec_output_t = nll_activation_fn(dec_output_t, self.config['nll_type'])
             decoded_list.append(dec_output_t.clone())
 
         return torch.cat(decoded_list, 0)