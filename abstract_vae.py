fix variance on [B, F]; return 0s for AE stuff

abstract_vae.py
@@ -35,6 +35,9 @@ class VarianceProjector(nn.Module):
                 # Expand the scalar variance to a matrix diagonal covariance and broadcast to 3 channels.
                 covariance = torch.eye(x.shape[-2], x.shape[-1], device=x.device) * self.variance_scalar
                 return torch.cat([x, covariance.expand_as(x)], 1)
+            elif x.dim() == 2:
+                # [B, F], expand to F
+                return torch.cat([x, self.variance_scalar.expand_as(x)], 1)
 
             raise Exception("unknown dims for Variance projector")
 
@@ -185,7 +188,7 @@ class AbstractVAE(nn.Module):
             z_samples = self.reparameterize_aggregate_posterior()
         else:
             z_samples = self.reparameterizer.prior(
-                    batch_size, scale_var=self.config['generative_scale_var'], **kwargs
+                batch_size, scale_var=self.config['generative_scale_var'], **kwargs
             )
 
         # in the normal case just decode and activate

autoencoder.py
@@ -86,7 +86,7 @@ class Autoencoder(SimpleVAE):
         # append the variance as necessary
         return self._append_variance_projection(decoder)
 
-    def loss_function(self, recon_x, x, reparam_map, **unused_kwargs):
+    def loss_function(self, recon_x, x, **unused_kwargs):
         """ Autoencoder is simple the NLL term in the VAE.
 
         :param recon_x: the unactivated reconstruction preds.
@@ -99,10 +99,12 @@ class Autoencoder(SimpleVAE):
         utils.nan_check_and_break(nll, "nll")
         return {
             'loss': nll,
+            'elbo': torch.zeros_like(nll),
             'loss_mean': torch.mean(nll),
-            'elbo_mean': torch.mean(torch.zeros_like(nll)),
+            'elbo_mean': 0,
             'nll_mean': torch.mean(nll),
-            'kld_mean': torch.mean(torch.zeros_like(nll)),
-            'proxy_mean': torch.mean(torch.zeros_like(nll)),
-            'mut_info_mean': torch.mean(torch.zeros_like(nll)),
+            'kld_mean': 0,
+            'kl_beta_scalar': 0,
+            'proxy_mean': 0,
+            'mut_info_mean': 0,
         }