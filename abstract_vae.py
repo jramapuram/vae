fixes for vrnn kl beta and AE

abstract_vae.py
@@ -369,7 +369,7 @@ class AbstractVAE(nn.Module):
         mut_info = self.mut_info(params, x.size(0))
 
         # get the kl-beta from the annealer or just set to fixed value
-        kl_beta = self.compute_kl_beta(self.config['kl_beta'])
+        kl_beta = self.compute_kl_beta([self.config['kl_beta']])[0]
 
         # sanity checks only dont in fp32 due to too much fp16 magic
         if not self.config['half']:

autoencoder.py
@@ -97,7 +97,9 @@ class Autoencoder(SimpleVAE):
 
         """
         nll = distributions.nll(x, recon_x, self.config['nll_type'])
-        utils.nan_check_and_break(nll, "nll")
+        if not self.config['half']:
+            utils.nan_check_and_break(nll, "nll")
+
         return {
             'loss': nll,
             'elbo': torch.zeros_like(nll),